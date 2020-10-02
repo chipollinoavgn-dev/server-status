b1a9c2ff0805d2e350fb47f5ea7a696cf0cca7b4
Yiğit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-10-02 09:58:46 +0300

    Update ping.php

--- ping.php
+++ ping.php
@@ -1,8 +1,8 @@
 <?php
 include_once "classes/incident.php";
-define(REMOTEHOST,"https://www.theskyfallen.com");
-define(REMOTEHOSTSERVICEID,"1");
+define(REMOTEHOST,"https://w2.theskyfallen.com");
+define(REMOTEHOSTSERVICEID,1);
 $rhc = file_get_contents(REMOTEHOST);
 if($rhc == ""){
-    Incident::addFromPing("1","Server Unreachable","Server is unreachable. We are trying to identify the problem.",REMOTEHOSTSERVICEID);
+    Incident::addFromPing(1,"Server Unreachable","Server is unreachable. We are trying to identify the problem.",REMOTEHOSTSERVICEID);
 }
